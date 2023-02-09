bfea83e01fdb7591562bc3aa41f4f5129cb6052a
Bando <bodorbodi@gmail.com>
2023-02-09 01:30:28 +0100

    Made stage 3 working.  - Needs improvement in detecting the bombs.  - Simplified the declarations  - Need a way to print and compare the tables

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -4,10 +4,33 @@ package minesweeper
 
 import kotlin.random.Random
 
+const val SAFE_CELL = "."
+const val MINE_CELL = "X"
+const val DEFAULT_FIELD_X = 9
+const val DEFAULT_FIELD_Y = 9
 
-class Minesweeper(private var mineNumber: Int){
+class Minesweeper(
+    private var sizeX: Int = DEFAULT_FIELD_X,
+    private var sizeY: Int = DEFAULT_FIELD_Y,
+    private var mineNumber: Int,
+    ){
 
-    private fun generateMines(): MutableList<Int> {
+    private val mineList = generateMineList()
+    private val mineField = makeMineField(mineList)
+    private val mineFieldWithNumbers = bombNearCount()
+
+
+    private fun makeMineField(list: MutableList<Int>): MutableList<MutableList<String>> {
+        val mineField = MutableList(sizeX){MutableList(sizeY){ SAFE_CELL} }
+        for (j in 0 until sizeX * sizeY) {
+            if(j in list) {
+                mineField[j/sizeX][j%sizeX] = MINE_CELL
+            }
+        }
+        return mineField
+    }
+
+    private fun generateMineList(): MutableList<Int> {
         val randomList = generateSequence {
             Random.nextInt(0,80)
         }
@@ -15,52 +38,127 @@ class Minesweeper(private var mineNumber: Int){
             .take(mineNumber)
             .sorted()
             .toMutableList()
-        //println(randomList)
         return randomList
     }
 
-
-    private fun makeTable(list: MutableList<Int>): MutableList<MutableList<String>> {
-        val table = mutableListOf(
-            mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
-            mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
-            mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
-            mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
-            mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
-            mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
-            mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
-            mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
-            mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),)
-        for (j in 0..80) {
-            if(j in list) {
-                table[j/9][j%9] = "X"
-            }
-        }
-        return table
-    }
-
-    private fun printTable(table: MutableList<MutableList<String>>){
-        for(i in 0 until 9) {
-            for (j in 0 until 9) {
-                print(table[i][j])
+    fun printMineField(){
+        for(i in 0 until sizeX) {
+            for (j in 0 until sizeY) {
+                print(mineField[i][j])
             }
             println()
         }
-
+        println()
     }
 
-    fun play(){
-        printTable(makeTable(generateMines()))
+    fun printMineFieldWithNumbers(){
+        for(i in 0 until sizeX) {
+            for (j in 0 until sizeY) {
+                print(mineFieldWithNumbers[i][j])
+            }
+            println()
+        }
+        println()
     }
-}
+
+
+    fun bombNearCount(): MutableList<MutableList<String>>{
+        var bombCount = 0
+        val mineFieldWithNumbers = mineField
+        for(row in 0 until sizeY) {
+            for (column in 0 until sizeX) {
+                //left-up
+                if(row == 0 && column == 0) {
+
+                    if(mineField[0][1] =="X") bombCount++
+                    if(mineField[1][1] == "X") bombCount++
+                    if(mineField[1][0] == "X") bombCount++
+                }
+                //right-up
+                else if(row == 0 && column == sizeY-1) {
+                    if(mineField[0][column-1] == "X") bombCount++
+                    if(mineField[1][column-1] == "X") bombCount++
+                    if(mineField[1][column] == "X") bombCount++
+                }
+                //right-down
+                else if(row == sizeX-1 && column == sizeY-1) {
+                    if(mineField[row][column-1] == "X") bombCount++
+                    if(mineField[row-1][column-1] == "X") bombCount++
+                    if(mineField[row-1][column] == "X") bombCount++
+                }
+                //left-down
+                else if (row == sizeX-1 && column == 0) {
+                    if(mineField[row][1] == "X") bombCount++
+                    if(mineField[row-1][1] == "X") bombCount++
+                    if(mineField[row-1][0] == "X") bombCount++
+                }
+
+                //left
+                else if (column == 0) {
+                    if(mineField[row-1][0] == "X") bombCount++
+                    if(mineField[row-1][1] == "X") bombCount++
+                    if(mineField[row][1] == "X") bombCount++
+                    if(mineField[row+1][1] == "X") bombCount++
+                    if(mineField[row+1][0] == "X") bombCount++
+                }
+                //bottom
+                else if (row == sizeY-1) {
+                    if(mineField[row][column-1] == "X") bombCount++
+                    if(mineField[row-1][column-1] == "X") bombCount++
+                    if(mineField[row-1][column] == "X") bombCount++
+                    if(mineField[row-1][column+1] == "X") bombCount++
+                    if(mineField[row][column+1] == "X") bombCount++
+
+                }
+                //right
+                else if (column == sizeX-1) {
+                    if(mineField[row-1][column] == "X") bombCount++
+                    if(mineField[row-1][column-1] == "X") bombCount++
+                    if(mineField[row][column-1] == "X") bombCount++
+                    if(mineField[row+1][column-1] == "X") bombCount++
+                    if(mineField[row+1][column] == "X") bombCount++
+
+                }
+                //top
+                else if (row == 0) {
+                    if(mineField[0][column-1] == "X") bombCount++
+                    if(mineField[1][column-1] == "X") bombCount++
+                    if(mineField[1][column] == "X") bombCount++
+                    if(mineField[1][column+1] == "X") bombCount++
+                    if(mineField[0][column+1] == "X") bombCount++
+                }
+                //middle
+                else {
+                    if(mineField[row][column-1] == "X") bombCount++
+                    if(mineField[row+1][column-1] == "X") bombCount++
+                    if(mineField[row+1][column] == "X") bombCount++
+                    if(mineField[row+1][column+1] == "X") bombCount++
+                    if(mineField[row][column+1] == "X") bombCount++
+                    if(mineField[row-1][column+1] == "X") bombCount++
+                    if(mineField[row-1][column] == "X") bombCount++
+                    if(mineField[row-1][column-1] == "X") bombCount++
+
+                    }
+
+                if(bombCount != 0 && mineFieldWithNumbers[row][column] != "X") {
+                    mineFieldWithNumbers[row][column] = bombCount.toString()
+                }
+                bombCount = 0
+                }
+
+            }
+        return mineFieldWithNumbers
+        }
+
+    }
 
 fun main() {
 
     println("How many mines do you want on the field?")
     val mineNumber = readLine()!!.toInt()
 
-    val game = Minesweeper(mineNumber)
-    game.play()
-
-
+    val game = Minesweeper(9, 9, mineNumber)
+    game.printMineField()
+    game.bombNearCount()
+    //game.printMineFieldWithNumbers()
 }
